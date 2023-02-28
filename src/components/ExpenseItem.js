48cbfa399d05d9f78f6e620d523abd62624ca7d1
Mamuni98 <nabanitasahoo468@gmail.com>
2023-02-28 13:26:15 +0530

    ReactStarting_Task_7

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,9 +4,9 @@ function ExpenseItem() {
       <div>
         <h2>Expense Items</h2>
       </div>
-      <div>Food Rs. 10</div>
-      <div> Petrol Rs. 100 </div>
-      <div>Movies Rs. 200</div>
+      <div>Food Rs 10</div>
+      <div> Petrol Rs 100 </div>
+      <div>Movies Rs 200</div>
     </div>
   );
 }
