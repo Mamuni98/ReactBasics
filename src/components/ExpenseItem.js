c9ad8e72d4582e9005b47796967286f0ceb64927
Mamuni98 <nabanitasahoo468@gmail.com>
2023-03-01 21:28:37 +0530

    Task-8

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,32 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 2 , 1);
+    const LocationOfExpenditure = ["Food", "Movies", "Petrol"];
+    const expenseAmount = [400, 600, 500];
   return (
     <div>
-      <div>
-        <h2>Expense Items</h2>
+      <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{LocationOfExpenditure[0]}</h2>
+          <div className="expense-item__price">Rs.{expenseAmount[0]}</div>
+        </div>
       </div>
-      <div>Food Rs 10</div>
-      <div> Petrol Rs 100 </div>
-      <div>Movies Rs 200</div>
+      <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{LocationOfExpenditure[1]}</h2>
+          <div className="expense-item__price">Rs.{expenseAmount[1]}</div>
+        </div>
+      </div>
+      <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{LocationOfExpenditure[2]}</h2>
+          <div className="expense-item__price">Rs.{expenseAmount[2]}</div>
+        </div>
+      </div>
     </div>
   );
 }
